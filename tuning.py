c2f281db11e95b9e9a56b6abeea10430c1323b48
Arborrrr <1316052360@qq.com>
2025-03-26 23:47:22 +0800

    Update tuning.py

--- tuning.py
+++ tuning.py
@@ -2,53 +2,68 @@ from data_preprocessing import *
 
 
 def tune_ElasticNet(trial):  # 优化ElasticNet的超参数
+    # 模型的最大迭代次数
     max_iter = trial.suggest_int("max_iter", 1000, 4000)
+    # ElasticNet 正则化项的系数，控制模型的复杂度
     alpha = trial.suggest_float("alpha", 1e-4, 1000, log=True)
+    # 模型中 L1 正则化和 L2 正则化的比例
     l1_ratio = trial.suggest_float("l1_ratio", 0.0, 1.0, step=0.05)
+    # 优化过程中的容忍度，决定了算法终止时的精度
     tol = trial.suggest_float("tol", 1e-6, 1e-3, log=True)
 
+    # 创建弹性回顾模型
     ElasticNet_regressor = ElasticNet(max_iter=max_iter, alpha=alpha, tol=tol, l1_ratio=l1_ratio, random_state=1)
 
-    # -- Make a pipeline
+    # 船舰一个管道，先进行线性预处理，在进行弹性回归
     ElasticNet_pipeline = make_pipeline(linear_preprocessor, ElasticNet_regressor)
 
-    ElasticNet_model = TransformedTargetRegressor(regressor=ElasticNet_pipeline, func=np.log1p, inverse_func=np.expm1)
+    # 进行变量变换
+    ElasticNet_model = TransformedTargetRegressor(
+        regressor=ElasticNet_pipeline,  # 选择需要变换的模型
+        func=np.log1p,                  # 对目标变量进行 log1p 变换(即对 y 使用 log(1 + y))
+        inverse_func=np.expm1           # 在预测后使用 expm1 函数来逆变换，得到原始的目标变量值(即 exp(y) - 1)
+    )
 
+    # sklearn中进行交叉验证的函数
     ss = ShuffleSplit(
-        n_splits=5,
-        test_size=0.2,
-        random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
+        n_splits=5,     # 进行5次交叉验证
+        test_size=0.2,  # 每次划分 20% 的数据作为测试集，剩余 80% 作为训练集。
+        random_state=0  # 确保每次划分数据时能获得相同的划分结果。
+    )
 
-    score = cross_val_score(ElasticNet_model, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
+    # 这是 sklearn 中的交叉验证函数，用来计算模型的交叉验证分数
+    score = cross_val_score(
+        ElasticNet_model,                          # 要评估的模型
+        pipe_data,                                 # 特征数据
+        y,                                         # 目标数据
+        scoring=make_scorer(mean_absolute_error),  # 评分标准是 平均绝对误差（MAE）
+        cv=ss                                      # 使用之前定义的 ShuffleSplit 进行交叉验证
+    )
+
+    # 计算 5 次交叉验证的得分，并返回平均得分作为模型的评估结果
     score = score.mean()
     return score
 
 
 def tune_xgboost_1(trial):  # 第一级xgboost优化
+    # 树的数量
     n_estimators1 = trial.suggest_int("n_estimators", 2000, 5000, step=500)
-
-    # L1 regularization weight
+    # L1 正则化的权重，使用对数尺度
     alpha1 = trial.suggest_float("alpha", 1e-8, 1.0, log=True)
-
-    # sampling ratio for training data
+    # 训练数据的子采样比率
     subsample1 = trial.suggest_float("subsample", 0.2, 1.0)
-
-    # sampling according to each tree
+    # 每棵树使用的特征比例
     colsample_bytree1 = trial.suggest_float("colsample_bytree", 0.4, 0.6)
-
-    # maximum depth of the tree, signifies complexity of the tree
+    # 树的最大深度
     max_depth1 = trial.suggest_int("max_depth", 3, 10, step=2)
-
-    # minimum child weight, larger the term more conservative the tree
+    # 最小叶节点样本权重
     min_child_weight1 = trial.suggest_int("min_child_weight", 1, 3)
-
-    # learning rate
+    # 学习率，使用对数尺度
     learning_rate1 = trial.suggest_float("learning_rate", 1e-6, 1, log=True)
-
-    # defines how selective algorithm is
+    # 树的分裂所需的最小损失减少值，使用对数尺度
     gamma1 = trial.suggest_float("gamma", 1e-8, 1.0, log=True)
 
+    # 建立XGBoost模型
     xgb_regressor = XGBRegressor(
         n_estimators=n_estimators1,
         alpha=alpha1,
@@ -62,14 +77,14 @@ def tune_xgboost_1(trial):  # 第一级xgboost优化
         random_state=1
     )
 
-    # -- Make a pipeline
+    # 建立管道，先进性树预处理，再进行XGBoost
     xgb_pipeline = make_pipeline(tree_preprocessor, xgb_regressor)
 
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
+    )
 
     score = cross_val_score(xgb_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
@@ -78,26 +93,12 @@ def tune_xgboost_1(trial):  # 第一级xgboost优化
 
 def tune_xgboost_2(trial):  # 第二级xgboost优化
     n_estimators1 = trial.suggest_int("n_estimators", 4000, 6000, step=500)
-
-    # L1 regularization weight
     alpha1 = trial.suggest_float("alpha", 1e-8, 1e-5, log=True)
-
-    # sampling ratio for training data
     subsample1 = trial.suggest_float("subsample", 0.3, 0.5)
-
-    # sampling according to each tree
     colsample_bytree1 = trial.suggest_float("colsample_bytree", 0.35, 0.56)
-
-    # maximum depth of the tree, signifies complexity of the tree
     max_depth1 = trial.suggest_int("max_depth", 3, 7, step=2)
-
-    # minimum child weight, larger the term more conservative the tree
     min_child_weight1 = trial.suggest_int("min_child_weight", 1, 3)
-
-    # learning rate
     learning_rate1 = trial.suggest_float("learning_rate", 1e-4, 1e-2, log=True)
-
-    # defines how selective algorithm is
     gamma1 = trial.suggest_float("gamma", 1e-3, 1e-1, log=True)
 
     xgb_regressor = XGBRegressor(
@@ -112,16 +113,12 @@ def tune_xgboost_2(trial):  # 第二级xgboost优化
         eval_metric='mae',
         random_state=1
     )
-
-    # -- Make a pipeline
     xgb_pipeline = make_pipeline(tree_preprocessor, xgb_regressor)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(xgb_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -129,26 +126,12 @@ def tune_xgboost_2(trial):  # 第二级xgboost优化
 
 def tune_xgboost_3(trial):  # 第三级xgboost优化
     n_estimators1 = trial.suggest_int("n_estimators", 6000, 7000, step=500)
-
-    # L1 regularization weight
     alpha1 = trial.suggest_float("alpha", 1e-9, 1e-7, log=True)
-
-    # sampling ratio for training data
     subsample1 = trial.suggest_float("subsample", 0.3, 0.4)
-
-    # sampling according to each tree
     colsample_bytree1 = trial.suggest_float("colsample_bytree", 0.25, 0.45)
-
-    # maximum depth of the tree, signifies complexity of the tree
     max_depth1 = trial.suggest_int("max_depth", 3, 7, step=2)
-
-    # minimum child weight, larger the term more conservative the tree
     min_child_weight1 = trial.suggest_int("min_child_weight", 1, 3)
-
-    # learning rate
     learning_rate1 = trial.suggest_float("learning_rate", 1e-3, 1e-2, log=True)
-
-    # defines how selective algorithm is
     gamma1 = trial.suggest_float("gamma", 1e-3, 1e-1, log=True)
 
     xgb_regressor = XGBRegressor(
@@ -163,27 +146,29 @@ def tune_xgboost_3(trial):  # 第三级xgboost优化
         eval_metric='mae',
         random_state=1
     )
-
-    # -- Make a pipeline
     xgb_pipeline = make_pipeline(tree_preprocessor, xgb_regressor)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(xgb_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
 
 
 def tune_gbm_1(trial):  # 第一级gbm优化
+    # 树的个数
     n_estimators = trial.suggest_int("n_estimators", 5000, 6000, step=500)
+    # 每个叶子节点的最小样本数
     min_samples_leaf = trial.suggest_int("min_samples_leaf", 10, 22, step=2)
+    # 树的最大深度
     max_depth = trial.suggest_int("max_depth", 2, 7, step=1)
+    # 学习率
     learning_rate = trial.suggest_float("learning_rate", 1e-3, 1e-2, log=True)
+    # 每棵树训练时随机选择的样本比例
     subsample = trial.suggest_float("subsample", 0.85, 0.95)
+    # 选择"sqrt"（特征数量的平方根）或"log2"（特征数量的对数）作为最大特征数
     max_features = trial.suggest_categorical("max_features", ["sqrt", "log2"])
 
     gbm_regressor = GradientBoostingRegressor(
@@ -195,16 +180,12 @@ def tune_gbm_1(trial):  # 第一级gbm优化
         max_features=max_features,
         random_state=1
     )
-
-    # -- Make a pipeline
     gbm_pipeline = make_pipeline(tree_preprocessor, gbm_regressor)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(gbm_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -227,16 +208,12 @@ def tune_gbm_2(trial):  # 第二级gbm优化
         max_features=max_features,
         random_state=1
     )
-
-    # -- Make a pipeline
     gbm_pipeline = make_pipeline(tree_preprocessor, gbm_regressor)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(gbm_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -244,13 +221,21 @@ def tune_gbm_2(trial):  # 第二级gbm优化
 
 def tune_catboost(trial):  # 优化catboost的超参数
     cat_param = {
+        # 迭代次数
         "iterations": trial.suggest_int("iterations", 4000, 6500, step=500),
+        # 每一层的列采样比例
         "colsample_bylevel": trial.suggest_float("colsample_bylevel", 0.02, 0.5),
+        # 学习率
         "learning_rate": trial.suggest_float("learning_rate", 1e-3, 1e-2, log=True),
+        # 随机强度，使用对数尺度优化
         "random_strength": trial.suggest_float("random_strength", 1e-2, 1, log=True),
+        # 树的深度
         "depth": trial.suggest_int("depth", 2, 12),
+        # L2 正则化项
         "l2_leaf_reg": trial.suggest_int("l2_leaf_reg", 1, 8),
+        # 使用的提升方法，只有一个选择 "Plain"（表示常规的提升方法）
         "boosting_type": trial.suggest_categorical("boosting_type", ["Plain"]),
+        # 数据采样方法，选择 "Bernoulli"（贝尔努利采样）
         "bootstrap_type": trial.suggest_categorical("bootstrap_type", ["Bernoulli"])
     }
 
@@ -259,17 +244,17 @@ def tune_catboost(trial):  # 优化catboost的超参数
     elif cat_param["bootstrap_type"] == "Bernoulli":
         cat_param["subsample"] = trial.suggest_float("subsample", 0.6, 1)
 
-    catboost_regressor = CatBoostRegressor(**cat_param, random_state=1, logging_level='Silent')
-
-    # -- Make a pipeline
+    catboost_regressor = CatBoostRegressor(
+        **cat_param,            # 解包超参数字典来初始化模型
+        random_state=1,         # 确保结果的可重现性
+        logging_level='Silent'  # 禁止日志输出
+    )
     catboost_pipeline = make_pipeline(tree_preprocessor, catboost_regressor)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(catboost_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -294,16 +279,12 @@ def tune_lightgbm_1(trial):  # 第一级lightgbm优化
         feature_fraction=feature_fraction,
         random_state=1
     )
-
-    # -- Make a pipeline
     lgbm_pipeline = make_pipeline(tree_preprocessor, lgbm_regressor)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(lgbm_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -328,16 +309,12 @@ def tune_lightgbm_2(trial):  # 第二级lightgbm优化
         feature_fraction=feature_fraction,
         random_state=1
     )
-
-    # -- Make a pipeline
     lgbm_pipeline = make_pipeline(tree_preprocessor, lgbm_regressor)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(lgbm_pipeline, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -354,18 +331,13 @@ def tune_Lasso(trial):  # 优化Lasso的超参数
         tol=tol,
         random_state=1
     )
-
-    # -- Make a pipeline
     Lasso_pipeline = make_pipeline(linear_preprocessor, Lasso_regressor)
-
     Lasso_model = TransformedTargetRegressor(regressor=Lasso_pipeline, func=np.log1p, inverse_func=np.expm1)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(Lasso_model, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -382,24 +354,19 @@ def tune_Ridge(trial):  # 优化Ridge的超参数
         tol=tol,
         random_state=1
     )
-
-    # -- Make a pipeline
     Ridge_pipeline = make_pipeline(linear_preprocessor, Ridge_regressor)
-
     Ridge_model = TransformedTargetRegressor(regressor=Ridge_pipeline, func=np.log1p, inverse_func=np.expm1)
-
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(Ridge_model, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
 
 
-# Be careful higher C values takes too long or fails to converge
+# C值过大可能导致收敛时间过长甚至无法收敛
 def tune_SVR_1(trial):  # 第一级SVR优化
     param = {
         "kernel": trial.suggest_categorical("kernel", ["linear", "rbf"]),
@@ -408,24 +375,17 @@ def tune_SVR_1(trial):  # 第一级SVR优化
         "tol": trial.suggest_float("tol", 1e-4, 1e-3, log=True)
     }
 
-    if param["kernel"] == "rbf":
-        gamma = trial.suggest_categorical("gamma", ["scale", "auto"])
-
     SVR_regressor = SVR(
         **param,
         cache_size=800
     )
-
-    # -- Make a pipeline
     SVR_pipeline = make_pipeline(linear_preprocessor, SVR_regressor)
-
     SVR_model = TransformedTargetRegressor(regressor=SVR_pipeline, func=np.log1p, inverse_func=np.expm1)
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(SVR_model, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
@@ -443,17 +403,13 @@ def tune_SVR_2(trial):  # 第二级SVR优化
         **param,
         cache_size=800
     )
-
-    # -- Make a pipeline
     SVR_pipeline = make_pipeline(linear_preprocessor, SVR_regressor)
-
     SVR_model = TransformedTargetRegressor(regressor=SVR_pipeline, func=np.log1p, inverse_func=np.expm1)
     ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
-    )  # ShuffleSplit will help you to keep distributions of training data and folds similar)
-
+    )
     score = cross_val_score(SVR_model, pipe_data, y, scoring=make_scorer(mean_absolute_error), cv=ss)
     score = score.mean()
     return score
