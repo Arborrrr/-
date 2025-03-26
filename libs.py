fd01b1896a62a88c4a85ecd1f502830c57f74951
Arborrrr <1316052360@qq.com>
2025-03-26 23:46:22 +0800

    Update libs.py

--- libs.py
+++ libs.py
@@ -1,25 +1,25 @@
-# basics
+# 基础库
 import numpy as np
 import pandas as pd
 import warnings
 
-# preprocessing
+# 预处理
 from sklearn.preprocessing import StandardScaler, PowerTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 import category_encoders as ce
 from sklearn.compose import TransformedTargetRegressor
 
-# feature engineering
+# 特征工程
 from sklearn.feature_selection import mutual_info_regression
 
-# transformers and pipeline
+# 变量转换和管道搭建
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn import set_config
 
-# algorithms
+# 算法库
 from xgboost import XGBRegressor
 from catboost import CatBoostRegressor
 from lightgbm import LGBMRegressor
@@ -27,7 +27,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import ElasticNet, Lasso, Ridge
 from sklearn.svm import SVR
 
-# model evaluation
+# 模型优化
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import ShuffleSplit
 from sklearn.metrics import mean_absolute_error, make_scorer
